51685c67eac9b413a19e9f89a9df944e21a0531a
SagarParmar-edelta <sagar.parmar@edeltaes.com>
2022-03-01 11:52:07 +0530

    Add some code in practice-app

--- React-node/practice-app/src/index.js
+++ React-node/practice-app/src/index.js
@@ -1,5 +1,16 @@
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import React from "react";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+function renderMyComp() {
+  const element = (
+    <div>
+      <h1>Welcome to React 🔥</h1>
+      <h2>It is {new Date().toLocaleTimeString()}</h2>
+    </div>
+  );
+  ReactDOM.render(element, document.getElementById("root"));
+}
+
+setInterval(renderMyComp, 1000);
